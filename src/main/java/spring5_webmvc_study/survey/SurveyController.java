1ff6463944d6434d14bb6456f2fd79150e2536be
mskim <net94@naver.com>
2021-05-21 09:20:07 +0900

    Model을 통해 컨트롤러에서 뷰에 데이터 전달하기

---
+++ src/main/java/spring5_webmvc_study/survey/SurveyController.java
@@ -0,0 +1,23 @@
+package spring5_webmvc_study.survey;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/survey")
+public class SurveyController {
+	@GetMapping
+    public String form() {
+        return "survey/surveyForm";
+    }
+
+    @PostMapping
+    public String submit(@ModelAttribute("ansData") AnsweredData data) {
+    	System.out.println("AnsweredData >> " + data);
+        return "survey/submitted";
+    }
+
+}
