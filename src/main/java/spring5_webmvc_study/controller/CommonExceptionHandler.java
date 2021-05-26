625109bc96278a05784f340484c0ba153a1e5274
inhwanK <inhan1009@naver.com>
2021-05-26 20:22:04 +0900

    예외처리 끝

--- src/main/java/spring5_webmvc_study/controller/CommonExceptionHandler.java
+++ src/main/java/spring5_webmvc_study/controller/CommonExceptionHandler.java
@@ -1,6 +1,5 @@
 package spring5_webmvc_study.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
